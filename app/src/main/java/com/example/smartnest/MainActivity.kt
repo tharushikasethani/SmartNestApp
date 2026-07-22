bc56eeb57190b330c70d8f5a7d62ce0459c5739f
RanduniMK <mokshard2003@gmail.com>
2026-07-22 12:51:14 +0530

    connect firebase database

--- app/src/main/java/com/example/smartnest/MainActivity.kt
+++ app/src/main/java/com/example/smartnest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.smartnest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,35 +14,86 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.smartnest.ui.theme.SmartNestTheme
 
+import com.google.firebase.database.FirebaseDatabase
+
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Firebase connection test
+        val database = FirebaseDatabase.getInstance()
+
+        val reference = database.getReference("test")
+
+        reference.setValue("Firebase Connected Successfully")
+            .addOnSuccessListener {
+
+                Toast.makeText(
+                    this,
+                    "Firebase Connected",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            .addOnFailureListener {
+
+                Toast.makeText(
+                    this,
+                    "Firebase Connection Failed",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+
+
         enableEdgeToEdge()
+
         setContent {
+
             SmartNestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
                     Greeting(
-                        name = "Android",
+                        name = "Smart Nest",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
+
             }
+
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+
     SmartNestTheme {
-        Greeting("Android")
+
+        Greeting("Smart Nest")
+
     }
+
 }
